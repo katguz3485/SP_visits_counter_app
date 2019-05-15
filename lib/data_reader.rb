d3ed3d4620346636248a9722d176388fd60497da
katguz3485 <katarzyna.guzik01@gmail.com>
2019-05-15 15:43:31 +0200

    reading file logic extracted to separate class

---
+++ lib/data_reader.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+class DataReader
+
+
+  attr_reader :logs
+
+  def initialize
+    @logs = logs
+  end
+
+  def logs
+    readed_logs(path)
+  end
+
+  private
+
+  def path
+    '/home/kasia/Desktop/visits_counter_app/lib/webserver.log.csv'
+  end
+
+  def readed_logs(path)
+    logs = []
+    CSV.foreach(path) do |row|
+      logs << row
+    end
+    logs
+  end
+
+end
+
+dr = DataReader.new
+puts dr.logs
